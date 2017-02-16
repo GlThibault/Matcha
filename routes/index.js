450118c920f0769d1550837feadece257e0f4639
Thibault GLANDAIS <tglandai@student.42.fr>
2017-02-16 16:55:00 +0100

    upd

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    if (req.session.error) {
-        res.locals.error = req.session.error
-        req.session.error = undefined}
-    if (req.session.success) {
-        res.locals.success = req.session.success
-        req.session.success = undefined}
-    res.locals.user = req.session.user
-    res.render('index', {
-        title: 'Matcha'
-    });
+    if (req.session && req.session.user) {
+        if (req.session.error) {
+            res.locals.error = req.session.error
+            req.session.error = undefined
+        }
+        if (req.session.success) {
+            res.locals.success = req.session.success
+            req.session.success = undefined
+        }
+        res.locals.user = req.session.user
+        res.render('index', {
+            title: 'Matcha'
+        });
+    } else {
+        res.redirect('/register');
+    }
 });
 
 module.exports = router;
--- routes/logout.js
+++ routes/logout.js
@@ -5,7 +5,6 @@ var session = require('express-session');
 router.get('/', function(req, res) {
     res.clearCookie(session.user);
     req.session.destroy();
-    req.session.success = "Vous êtes maintenant déconnecté";
     res.redirect('/')
 });
 
