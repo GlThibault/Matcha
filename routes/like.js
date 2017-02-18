9d40460724d5916924eaf09b99f17d3429ddc22d
Thibault GLANDAIS <tglandai@student.42.fr>
2017-02-18 17:07:38 +0100

    likes WIP

--- routes/like.js
+++ routes/like.js
@@ -5,12 +5,22 @@ var connection = require('../config/db');
 
 router.get('/:username', function(req, res, next) {
     if (req.session && req.session.user) {
-      connection.query('INSERT INTO likes SET username = ?, liked = ?', [req.session.user, req.params.username], (err, result) => {
-          if (err) throw err
-          else {
-            res.redirect('../u/' + req.params.username);
-          }
-      })
+        connection.query('SELECT COUNT(*) AS count FROM likes WHERE username = ? AND liked = ?', [req.session.user, req.params.username], (err, result) => {
+            if (err) throw err
+            if (result[0].count == 0) {
+                connection.query('INSERT INTO likes SET username = ?, liked = ?', [req.session.user, req.params.username], (err, result) => {
+                    if (err) throw err
+                    else
+                        res.redirect('../u/' + req.params.username);
+                })
+            } else {
+                connection.query('DELETE FROM likes WHERE username = ? AND liked = ?', [req.session.user, req.params.username], (err, result) => {
+                    if (err) throw err
+                    else
+                      res.redirect('../u/' + req.params.username);
+                })
+            }
+        })
     } else {
         req.session.error = "Vous devez être connecté pour accéder a cette page.";
         res.redirect('/login');
--- routes/u.js
+++ routes/u.js
@@ -6,18 +6,24 @@ router.get('/', function(req, res, next) {
     if (req.session && req.session.user) {
         if (req.session.error) {
             res.locals.error = req.session.error
-            req.session.error = undefined}
+            req.session.error = undefined
+        }
         if (req.session.success) {
             res.locals.success = req.session.success
-            req.session.success = undefined}
-        connection.query('SELECT * FROM users WHERE bio != \'NULL\'', (err, rows, result) => {
-              if (err) throw err
-              res.locals.user = req.session.user
-              res.locals.rows = rows
-              res.render('u', {
-                  title: 'Utilisateurs'
-              });
-          });
+            req.session.success = undefined
+        }
+        connection.query('SELECT username, COUNT(DISTINCT username) AS count FROM likes GROUP BY username', (err, rows, result) => {
+            if (err) throw err
+            res.locals.likes = rows
+            connection.query('SELECT * FROM users WHERE bio != \'NULL\'', (err, rows, result) => {
+                if (err) throw err
+                res.locals.user = req.session.user
+                res.locals.rows = rows
+                res.render('u', {
+                    title: 'Utilisateurs'
+                });
+            });
+        });
     } else {
         req.session.error = "Vous devez être connecté pour accéder a cette page.";
         res.redirect('/login');
@@ -30,17 +36,17 @@ router.get('/:username', function(req, res, next) {
             res.locals.error = req.session.error
             req.session.error = undefined
         }
-        connection.query('SELECT * FROM likes WHERE username = ? AND liked = ?', [req.params.username, req.session.user], (err, rows, result) => {
+        connection.query('SELECT COUNT(*) AS count FROM likes WHERE username = ? AND liked = ?', [req.session.user, req.params.username], (err, result) => {
             if (err) throw err
-            res.locals.likes = rows[0]
-          connection.query('SELECT * FROM users WHERE username = ?', [req.params.username], (err, rows, result) => {
-              if (err) throw err
-              res.locals.user = req.session.user
-              res.locals.data = rows[0]
-              res.render('user', {
-                title: rows[0]['lastname'] + " " + rows[0]['firstname']
-              });
-          });
+            res.locals.likes = result[0].count
+            connection.query('SELECT * FROM users WHERE username = ?', [req.params.username], (err, rows, result) => {
+                if (err) throw err
+                res.locals.user = req.session.user
+                res.locals.data = rows[0]
+                res.render('user', {
+                    title: rows[0]['lastname'] + " " + rows[0]['firstname']
+                });
+            });
         });
     } else {
         req.session.error = "Vous devez être connecté pour accéder a cette page.";
